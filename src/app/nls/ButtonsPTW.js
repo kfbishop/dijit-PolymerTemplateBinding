8ee2f6d35362fb761a441052aba931921694f2b3
kfbishop <kfbishop@gmail.com>
2013-08-22 13:48:32 -0400

    gh-pages: Duplicate master docs tree

--- src/app/nls/ButtonsPTW.js
+++
@@ -1,9 +0,0 @@
-/*global define*/
-define({
-    root: {
-        title       : "Buttons - Using Polymer Template Widget",
-        addButton   : "Add Another Button",
-        changeColor : "Change Header Color",
-        toggleH2    : "Toggle showing secondary header"
-    }
-});
